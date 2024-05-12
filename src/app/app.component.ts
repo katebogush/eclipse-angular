728681e8dfd30af6614824bdba7acbdf7aa68b8c
KaterynaBogush <kate.bogush89@gmail.com>
2024-05-12 15:20:30 +0300

    new sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent} from './components/UI/header/header.component';
 import { SidebarComponent } from './components/UI/sidebar/sidebar.component';
-
+import { FooterComponent } from './components/UI/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/UI/header/header.component.ts
+++ src/app/components/UI/header/header.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBasketShopping } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  faUser = faUser
+  faBasket=faBasketShopping;
+  faMagnifyingGlass =faMagnifyingGlass;
 
 }
